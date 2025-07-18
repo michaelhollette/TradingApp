c6509fb095b448f9f14ec7e8eedca6f7b214b2e9
mh2011-maisie <mh2011.music@gmail.com>
2025-07-18 18:32:24 +0100

    containerized with docker compose

---
+++ python-backend/market_data.py
@@ -0,0 +1,5 @@
+class MarketDataService():
+    def __init__(self):
+        api_key = "FGkzWV4lrs1pDemA6kxNLzE7PdY4elEq"
+        api_key2 = "tXI3IbsvZVPvZhdlB7iyGUbf4YYQJKiZ"
+        api_key3="GhhSe5dXT7x8Sxf71TuPFccL8Ofx0c0b"
